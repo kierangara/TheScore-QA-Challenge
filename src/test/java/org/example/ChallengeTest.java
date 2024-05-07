04b6cbd08585e14d52ec6bc4a952e2beeadcc75c
Kieran Gara <kierangara@gmail.com>
2024-05-07 03:24:06 -0400

    Established application flow for test

--- src/test/java/org/example/ChallengeTest.java
+++ src/test/java/org/example/ChallengeTest.java
@@ -10,6 +10,10 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.Assert;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 import java.net.MalformedURLException;
 
 
@@ -23,17 +27,27 @@ public class ChallengeTest extends TestBase {
         driver = getDriver();
         var el1 = driver.findElement(AppiumBy.accessibilityId("Predicted app: theScore"));
         el1.click();
-        /*var el4 = driver.findElement(AppiumBy.id("com.fivemobile.thescore:id/search_bar_text_view"));
+        waitForElement(driver, "//android.widget.TextView[@resource-id=\"com.fivemobile.thescore:id/search_bar_text_view\"]");
+        var el4 = driver.findElement(AppiumBy.id("com.fivemobile.thescore:id/search_bar_text_view"));
         el4.click();
+        waitForElement(driver, "//android.widget.AutoCompleteTextView[@resource-id=\"com.fivemobile.thescore:id/search_src_text\"]");
         var el5 = driver.findElement(AppiumBy.id("com.fivemobile.thescore:id/search_src_text"));
         el5.sendKeys("Bayern");
         var el6 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.LinearLayout\").instance(11)"));
         el6.click();
+        waitForElement(driver, "//android.widget.TextView[@text=\"TEAM STATS\"]");
         var el7 = driver.findElement(AppiumBy.accessibilityId("Team Stats"));
         el7.click();
+        waitForElement(driver, "//android.widget.ImageButton[@content-desc=\"Navigate up\"]");
         var el8 = driver.findElement(AppiumBy.accessibilityId("Navigate up"));
-        el8.click();*/
+        el8.click();
         Assert.assertEquals(4,2+2);
+    }
+
+    public void waitForElement(AndroidDriver driver, String xpath){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(By
+                .xpath(xpath)));
     }
 
 }
